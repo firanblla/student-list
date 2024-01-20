104899355b770b6cedaa06d8fe1ab0d39f6faaf1
firanblla <firanblla13@gmail.com>
2024-01-20 20:55:13 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:tb2_mobile/models/student.dart';
+import 'package:tb2_mobile/utils/home_screen.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(StudentAdapter());
+  await Hive.openBox<Student>('students');
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Course Reminder',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomeScreen(),
+        debugShowCheckedModeBanner:false,
+    );
+  }
+}
